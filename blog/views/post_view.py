a71666395090b1f411305a3b6fd86c42c66a2143
lucas menezes <menezeslucas24082002@gmail.com>
2025-08-15 10:28:04 -0300

    Fix: correção no post_view.py para vizualizar posts na home

--- blog/views/post_view.py
+++ blog/views/post_view.py
@@ -3,7 +3,7 @@ from django.views import generic
 from blog.models import Post
 
 class PostView(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    queryset = Post.objects.filter(status='published').order_by('-created_on')
     template_name = 'index.html'
 
 
